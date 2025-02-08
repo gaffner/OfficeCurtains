fb1d8eb941aae177071cbee4b61e2c2c26a89d85
Gefen Altshuler <gefen102@gmail.com>
2025-02-08 15:21:35 +0200

    Added statistics

--- config.py
+++ config.py
@@ -5,6 +5,10 @@ CURTAINS_USERNAME = os.getenv('CURTAINS_USERNAME')
 MD5_VALUE = os.getenv('MD5_VALUE')
 CURTAINS_PASSWORD = os.getenv('CURTAINS_PASSWORD')
 REPORTS_FILE = os.getenv('REPORTS_FILE')
+STATISTICS_FILE = os.getenv('STATISTICS_FILE')
+STATISTICS_FOLDER = "csv/"
+CSV_FORMAT = "Room,Up,Down"
+
 
 def get_server_port(suffix):
     return os.getenv('SERVER_PORT_' + suffix)
--- statistics.py
+++ statistics.py
@@ -1,2 +1,25 @@
-def log_control(room_name: str, direction: str):
-    with open
+import hashlib
+import os
+from datetime import datetime
+
+from fastapi import Request
+
+from config import STATISTICS_FOLDER, CSV_FORMAT
+from utils import get_client_ip
+
+
+def get_current_csv():
+    csv_name = datetime.now().strftime("%Y-%m-%d")
+    csv = open(os.path.join(STATISTICS_FOLDER, csv_name), "a")
+    csv.write(CSV_FORMAT)
+
+    return csv
+
+
+def log_control(request: Request, room_name: str, direction: str):
+    hashed_user_ip = hashlib.sha1(get_client_ip(request).encode()).hexdigest()
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    report_entry = f"{room_name},{hashed_user_ip},{direction}\n"
+
+    csv = get_current_csv()
+    csv.write(report_entry)
