1c3ff311254802cf55f90074450e86642667b141
Gefen Altshuler <gefen102@gmail.com>
2025-12-06 14:12:11 +0200

    Created helper.py

--- server.py
+++ server.py
@@ -1,15 +1,13 @@
-import json
 import logging
 from datetime import datetime
-from functools import lru_cache
 
-import requests
 import uvicorn
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import RedirectResponse
 from starlette.staticfiles import StaticFiles
 
 from config import *
+from helper import get_suffix, get_username, get_states_by_direction, send_message, get_room_states
 from statistics import StatisticsManager
 from utils import validate_isp, get_client_ip
 
@@ -20,7 +18,6 @@ load_dotenv()
 app = FastAPI(redirect_slashes=False)
 
 # Constants for the server and authentication
-
 
 app.mount("/Frontend", StaticFiles(directory="Frontend"), name="Frontend")
 
@@ -56,37 +53,6 @@ def submit_report(request: Request, content: str):
 def root(request: Request):
     return RedirectResponse(url="/Frontend/index.html")
 
-
-# Function to load room data from 'rooms.json' file
-@lru_cache(maxsize=None)
-def load_rooms_data():
-    try:
-        with open('rooms.json', 'r', encoding='utf-8') as file:
-            return json.load(file)
-    except Exception as e:
-        logging.error(f"Error loading rooms.json: {e}")
-        return {}
-
-
-# Function to send POST requests to the server
-def send_message(group, command, creds, address):
-    url = f"https://{address[0]}:{address[1]}/iphone/send"
-    data = f"username={creds[0]}\r\npassword={creds[1]}\r\nsk=\r\nversion=2\r\nmd5={MD5_VALUE}\r\ngroup={group}\r\neis=1.001\r\nvalue={command}\r\n"
-    logging.info(f'Posting to: {url} with data: {data}')
-
-    res = requests.post(url, data=data, headers={'User-Agent': 'XXter/1.0'}, verify=False)
-    return res
-
-
-def get_room_states(room_name: str):
-    rooms = load_rooms_data()
-
-    if room_name not in rooms:
-        raise HTTPException(status_code=404, detail="Room not found")
-
-    return rooms[room_name]
-
-
 @app.get("/register/{room_name}")
 @validate_isp()
 def register(request: Request, room_name: str):
@@ -94,34 +60,6 @@ def register(request: Request, room_name: str):
     directions = [state['name'] for state in states]
 
     return directions
-
-
-def get_suffix(room_name):
-    suffix = room_name[1]
-    if suffix not in ('A', 'B', 'C'):
-        raise HTTPException(status_code=404, detail=f"incorrect building {suffix}")
-
-    return suffix
-
-
-def get_username(room_name):
-    suffix = get_suffix(room_name)
-    username = CURTAINS_USERNAME + suffix
-    logging.info(f'username is {username}')
-
-    return username
-
-
-def get_states_by_direction(room_name, direction):
-    states = get_room_states(room_name)
-    # if this room have multiple directions, get the correct one
-    if direction and len(states) > 1:
-        for state in states:
-            if state['name'] == direction:
-                return state
-
-    # if not, return the only direction exists
-    return states[0]
 
 
 @app.get("/control/{room_name}/{action}")
